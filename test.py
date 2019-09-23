2ff289b23d4a832903f187c04ca285ec4113db1a
bimoadityar <bimo.awm1@gmail.com>
2019-09-23 23:24:39 +0700

    Create random failure test.py

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 from filepacket import FilePacketSender, FilePacketReciever
+import random
 
 if __name__ == "__main__":
     sender = FilePacketSender("testImage.jpg", 0)
     reciever = FilePacketReciever("testImage2.jpg", 0)
     while not sender.is_done():
         packet = sender.send_packet()
+        if bool(random.getrandbits(1)):
+            packet = b''
         # print(packet)
         if reciever.recieve_packet(packet):
             ack = reciever.send_ack()
-            sender.recieve_ack(ack)
+            if bool(random.getrandbits(1)):
+                ack = b''
+            if not sender.recieve_ack(ack):
+                print("recieved bad ack")
+        else:
+            print("recieved bad packet")
     print("done")
