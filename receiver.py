742289270c176bd0415a8ff36dd2f0150058b7f8
bimoadityar <bimo.awm1@gmail.com>
2019-09-24 08:57:28 +0700

    Fix receiver

--- receiver.py
+++ receiver.py
@@ -6,11 +6,10 @@ class Receiver:
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._ip = socket.gethostbyname(socket.gethostname()) # get current computer IP address
-        self._socket.bind((ip, 0)) # automatically find open port to connect to
-        self._port = self.socket.getsockname()[1]
-        print("Reciever active on", ip, "port", port)
+        self._socket.bind((self._ip, 0)) # automatically find open port to connect to
+        self._port = self._socket.getsockname()[1]
+        print("Reciever active on", self._ip, "port", self._port)
         self._writer = {}
-        self._ret_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # recieve packet from (ip, port) and distribute it to the corresponding file writer
     def run(self):
@@ -19,20 +18,22 @@ class Receiver:
             id = get_id(packet)
             if id < 0:
                 continue
-            idd = ret_addr + "-" + str(id)
+            idd = ret_addr[0] + "-" + str(ret_addr[1]) + "-" + str(id)
             if idd not in self._writer:
-                self._writer[idd] = FilePacketWriter(idd, id)
+                self._writer[idd] = FilePacketReceiver(idd, id)
         if self._writer[idd].recieve_packet(packet):
             seq = self._writer[id].rcv_seq()
             print("Recieved packet", idd, "seq", seq)
-            self._ret_socket.sendto(self._writer[idd].send_ack(), (ret_addr, self._port))
+            self._ret_socket.sendto(self._writer[idd].send_ack(), ret_addr)
         else:
             print("Recieved bad packet from", ret_addr)
 
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('localhost', 10000))
-    print("Reciever active on", socket.gethostbyname(socket.gethostname()), "port", sock.getsockname()[1])
-    while True:
-        packet, ret_addr = sock.recvfrom(1024)
-        print("Recieved", packet.decode(), "bytes from", ret_addr[0], "port", ret_addr[1])
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.bind(('localhost', 10000))
+    # print("Reciever active on", socket.gethostbyname(socket.gethostname()), "port", sock.getsockname()[1])
+    # while True:
+    #     packet, ret_addr = sock.recvfrom(1024)
+    #     print("Recieved", packet.decode(), "bytes from", ret_addr[0], "port", ret_addr[1])
+    rec = Receiver()
+    rec.run()
--- test.py
+++ test.py
@@ -1,16 +1,22 @@
-from filepacket import FilePacketSender, FilePacketReciever
+from filepacket import FilePacketSender, FilePacketReceiver
 import random
 
 if __name__ == "__main__":
     sender = FilePacketSender("testImage.jpg", 0)
-    reciever = FilePacketReciever("testImage2.jpg", 0)
+    reciever = FilePacketReceiver("testImage2.jpg", 0)
     while not sender.is_done():
         packet = sender.send_packet()
         if random.randrange(100) < 35:
-            packet[4] = 255 - packet[2]
+            for i in range(len(packet)):
+                if random.randrange(100) < 5:
+                    packet[i] = random.randrange(256)
         # print(packet)
         if reciever.recieve_packet(packet):
             ack = reciever.send_ack()
+            if random.randrange(100) < 35:
+                for i in range(len(ack)):
+                    if random.randrange(100) < 5:
+                        ack[i] = random.randrange(256)
             if not sender.recieve_ack(ack):
                 print("recieved bad ack")
         else:
