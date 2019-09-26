2008373bfa5ec27d75556be7287c8566c718a915
Ariel Ansa Razumardi <arielrazumardi@gmail.com>
2019-09-26 20:47:29 +0700

    Fixed bugs and cleaned code a bit

--- sender.py
+++ sender.py
@@ -2,11 +2,9 @@ import threading
 import socket
 from filepacket import FilePacketSender
 import time
-import string
-import random
 import sys
 import os
-import math
+# class for a thread that runs function with args every time seconds
 class ThreadTimer(threading.Thread):
     def __init__(self, event, time, function, args):
         threading.Thread.__init__(self)
@@ -19,24 +17,23 @@ class ThreadTimer(threading.Thread):
         while not self.stopped.wait(self.time):
             self.function(self.args)
 
+# class for reading data from file named filename and send packet to (ip, port)
 class Sender:
     def __init__(self, ip, port, filename, id):
         self._receiver_ip = ip
         self._receiver_port = port
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._reader = FilePacketSender(filename, id)
+        self._id = id
         self.filesize = os.stat(filename).st_size
         self.partsize = 0
         global progress
 
+    # send MESSAGE to (ip, port)
     def send_message(self,MESSAGE):
         self._socket.sendto(MESSAGE, (self._receiver_ip, self._receiver_port))
-        self.partsize = (self._reader.get_rcv_seq() + 1) * 32768
-        if self.partsize > self.filesize:
-            self.partsize = self.filesize
-        progress[id] = self.partsize/self.filesize
         
-
+    # read data from file named filename and send packet to (ip, port)
     def run(self):
         while not(self._reader.is_done()):
             stop_flag = threading.Event()
@@ -47,41 +44,52 @@ class Sender:
             while not(self._reader.receive_ack(ack)):
                 ack, _ = self._socket.recvfrom(1024)
             stop_flag.set()
+            self.partsize = (self._reader.get_rcv_seq() + 1) * 32768
+            if self.partsize > self.filesize:
+                self.partsize = self.filesize
+            progress[self._id] = self.partsize/self.filesize
         
 
-
+# thread for a sender for each file
 def file_sender_thread(ip, port, filepath, id):
     sender = Sender(ip, port, filepath, id)
     sender.run()
     print(ip, port, filepath, id)
 
-def progresbar(): 
-    for i in range(21):
-        sys.stdout.write('\r')
-        sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
-        time.sleep(0.01)
-    print()
+
+# retuns True if all progress has ended
+def progress_ended(progress, n):
+    end = True
+    for i in range(n):
+        if progress[i] < 1:
+            end = False
+    return end
 
 if __name__ == "__main__":
     UDP_IP = input("Insert receiver IP   : ")
     UDP_PORT = int(input("Insert receiver port : "))
     n = int(input("Insert number of files to send: "))
+
     thread = []
     filepath = []
+
     global progress
     progress = []
+
     for i in range(n):
-        thread.append('')
-        progress.append('0')
+        progress.append(0)
         filepath.append(input('Insert Filepath #' + str(i) + ': '))
-        thread[i] = threading.Thread(target=file_sender_thread, args=(UDP_IP,  UDP_PORT, filepath[i], i))
+        thread.append(threading.Thread(target=file_sender_thread, args=(UDP_IP,  UDP_PORT, filepath[i], i)))
+
     for i in range(n):
         thread[i].start()
-    while True: 
+
+    while not progress_ended(progress, n): 
         if os.name =='nt':
             os.system('cls')
         else:
             os.system('clear')
+
         for i in range(n):
             sys.stdout.write("[%-20s]" % ('='*int(progress[i]*20)))
             sys.stdout.write(str(int(progress[i]*100))+"%")
@@ -89,8 +97,8 @@ if __name__ == "__main__":
             sys.stdout.write("   " + filepath[i])
             sys.stdout.write('\n')
             sys.stdout.flush()
-        time.sleep(0.01)
         
+    sys.stdout.write("All files successfully sent\n")
         
     
 
